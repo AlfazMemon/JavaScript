8af0b522417bd10fb3eb40123bdb2a2da2c92462
memon alfaz <memonalfaz749@gmail.com>
2026-05-26 17:50:33 +0530

    product-managment

---
+++ 07_typescript.js
@@ -0,0 +1,4 @@
+"use strict";
+let studentName = "Alfaz memon";
+let age = 19;
+console.log("TypeScript working!");
---
+++ 07_typescript.ts
@@ -0,0 +1,5 @@
+let studentname : string = "Alfaz memon";
+
+let Age : number = 19;
+
+console.log("TypeScript working!");
