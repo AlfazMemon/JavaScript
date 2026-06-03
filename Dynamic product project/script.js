6f78af8cdcec4d49c3ff90aa1a159eaefa623796
memon alfaz <memonalfaz749@gmail.com>
2026-06-03 20:02:53 +0530

    project

---
+++ Dynamic product project/script.js
@@ -0,0 +1,10 @@
+const products = [
+   {
+    name: "Wireless Mouse",
+    desc: "Ergonomic wireless mouse with smooth tracking.",
+    price: 799,
+    category: "Electronics",
+    image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTeDOKovc88AP1nOv2dI0ZW8ofbbn35k9a5VBl5bMkSErpAQahvlh5r_huYLG_lqX8aOcttm56LSTjTKyigV-L94S9nIysT-HKsntpnNFJEhHKUeVNmGxMRjTY"
+  },
+];
+
