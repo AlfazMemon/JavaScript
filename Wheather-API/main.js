dbd59aee9056f13e749f9af95d35dcc378de1745
memon alfaz <memonalfaz749@gmail.com>
2026-07-10 21:56:47 +0530

    project

--- Wheather-API/main.js
+++ Wheather-API/main.js
@@ -4,23 +4,56 @@ function FetchWeather() {
 
     fetch("http://localhost:3000/cities")
         .then((res) => res.json())
-        .then(data => {
+        .then((data) => {
+            Display(data, city)
 
-            let weather = data.find(item =>
-                item.city.toLowerCase() === city
-            );
 
-            if (!weather) {
-                alert("City not found");
-                return;
-            }
-            
-            document.getElementById("cityName").innerText = weather.city;
-            document.getElementById("temp").innerText = weather.temperature + "°C";
-            document.getElementById("condition").innerText = weather.condition;
-            document.getElementById("feels").innerText = weather.feels_like + "°C";
-            document.getElementById("humidity").innerText = weather.humidity + "%";
-            document.getElementById("wind").innerText = weather.wind_kph + " km/h";
         })
         .catch(error => console.log(error));
 }
+function Display(data, city) {
+    let weather = data.find(item =>
+        item.city.toLowerCase() === city
+    );
+
+    if (!weather) {
+        alert("City not found");
+        return;
+    }
+
+    let store = `
+    <div id="weather-card" class="card">
+
+            <h2 id="cityName">${weather.city}</h2>
+
+            <h1 id="temp">${weather.temperature}°C</h1>
+
+            <h3 id="condition">${weather.condition}</h3>
+
+            <div class="details">
+    <div>
+        <p>Feels Like</p>
+        <h4>${weather.feels_like}°C</h4>
+    </div>
+
+    <div>
+        <p>Humidity</p>
+        <h4>${weather.humidity}%</h4>
+    </div>
+
+    <div>
+        <p>Wind</p>
+        <h4>${weather.wind_kph} km/h</h4>
+    </div>
+</div>
+
+        </div>
+`;
+
+    document.getElementById('result').innerHTML = store;
+}
+
+window.addEventListener('DOMContentLoaded',()=>{
+    document.getElementById("city").value = "Ahmedabad"
+    FetchWeather();
+})
