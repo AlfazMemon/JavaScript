366d2496179fb5338b3e634c97c6b33b6fccd447
memon alfaz <memonalfaz749@gmail.com>
2026-07-20 22:10:32 +0530

    note app commit

--- daily-task/DOM-assignment-1/day-4/script.js
+++ daily-task/DOM-assignment-1/day-4/script.js
@@ -1,86 +1,104 @@
 
 let btn = document.getElementById('addBtn');
 let notes = document.getElementById('notesContainer');
+let editIndex = null;
 
 let arr = JSON.parse(localStorage.getItem("Notes")) || [];
 
-window.addEventListener('load',()=>{
+window.addEventListener('load', () => {
     Display();
 })
-function NewNotes(){
+function NewNotes() {
 
     let title = document.getElementById('title').value;
     let description = document.getElementById('description').value;
-    
+
 
     if (title === "" || description === "") {
-      alert("Please fill all fileds");
-      return
+        alert("Please fill all fileds");
+        return
     }
 
-          const note = {
-            title,
-            description
-        }
+    const note = {
+        title,
+        description
+    }
 
+    if (editIndex === null) {
         arr.push(note);
+    } else {
+        arr[editIndex] = note;
+        editIndex = null;
 
-        localStorage.setItem("Notes",JSON.stringify(arr));
+        document.getElementById("addBtn").innerText = "Add Note";
+    }
 
-        Display();
-        document.getElementById('title').value = "";
-        document.getElementById('description').value = "";
+    localStorage.setItem("Notes", JSON.stringify(arr));
+
+    Display();
+    document.getElementById('title').value = "";
+    document.getElementById('description').value = "";
 
 }
 
 
 function Display(data = arr) {
 
-   
-        notes.innerHTML = "";
 
-        data.forEach((ele, index) => {
-            let store = `
+    notes.innerHTML = "";
+
+    data.forEach((ele, index) => {
+        let store = `
             <div class="card">
                 <h2>${ele.title}</h2>
                 <p>${ele.description}</p>
-                <button onclick="Delete(${index})" class="delete">Delete</button>
+               <button onclick="edit(${index})" class="edit">Edit</button>
+               <button onclick="Delete(${index})" class="delete">Delete</button>
             </div>
           `
-            notes.innerHTML += store;
-        });
+        notes.innerHTML += store;
+    });
 
-        
+
 }
 
 
 
-function Delete(index){
+function Delete(index) {
 
-    arr.splice(index,1);
+    arr.splice(index, 1);
 
-    localStorage.setItem("Notes",JSON.stringify(arr));
-    
+    localStorage.setItem("Notes", JSON.stringify(arr));
+
     Display();
 }
 
-function Search(){
-    
+function Search() {
+
     let searchInput = document.getElementById('search').value.toLowerCase();
 
-    if(searchInput === ""){
+    if (searchInput === "") {
 
         Display()
-        return  
+        return
     }
-    else{
-        let abc = arr.filter((el)=>el.title.toLowerCase().includes(searchInput));
+    else {
+        let abc = arr.filter((el) => el.title.toLowerCase().includes(searchInput));
         Display(abc)
     }
 }
 
-function edit(){
+function edit(index) {
 
-    
-    
+    let note = arr[index];
+
+    document.getElementById('title').value = note.title;
+    document.getElementById('description').value = note.description;
+
+    editIndex = index;
+
+    document.getElementById('addBtn').innerHTML = "Update Note"
+
+
+
 }
